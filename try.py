fedacfaa3532148ab4fcef4e2d1b0fb206fd84b3
AvinashVunnava <avinash@deepcompute.com>
2017-12-10 21:46:25 +0530

    changes were made

--- try.py
+++ try.py
@@ -2,12 +2,12 @@ def oneover(j):
     return(1/j)
 try:
     i = oneover('x')
-    print(i)
+    print i
 except ZeroDivisionError as e:
-    print('An error occured: %s' % e)
+    print 'An error occured: %s' % e
 except TypeError as e:
-    print ('can divide only integers: %s' % e)
+    print 'can divide only integers: %s' % e
 else:
-    print('No Exception Occured')
+    print 'No Exception Occured'
 finally:
-    print('this always executed')
+    print 'this always executed'
